57408195053cc7564ea8bb73f524a74d25a35279
“lincgcg <chunganglin@gmail.com>
2023-10-18 22:57:12 +0800

    debug

--- 12_woDCS_GRU.sh
+++ 12_woDCS_GRU.sh
@@ -2,139 +2,139 @@
 
 # 01
 
-CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/LSTM/woDCS/01/data.traffic --log_filename /root/EBSNN-file/12/LSTM/woDCS/01/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/12/LSTM/woDCS/01/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/GRU/woDCS/01/data.traffic --log_filename /root/EBSNN-file/12/GRU/woDCS/01/log.txt  --model EBSNN_GRU --epochs 10 --csv_file /root/EBSNN-file/12/GRU/woDCS/01/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
 
 
 
 
 # 02
 
-CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/LSTM/woDCS/02/data.traffic --log_filename /root/EBSNN-file/12/LSTM/woDCS/02/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/12/LSTM/woDCS/02/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/GRU/woDCS/02/data.traffic --log_filename /root/EBSNN-file/12/GRU/woDCS/02/log.txt  --model EBSNN_GRU --epochs 10 --csv_file /root/EBSNN-file/12/GRU/woDCS/02/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
 
 
 
 
 # 03
 
-CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/LSTM/woDCS/03/data.traffic --log_filename /root/EBSNN-file/12/LSTM/woDCS/03/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/12/LSTM/woDCS/03/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/GRU/woDCS/03/data.traffic --log_filename /root/EBSNN-file/12/GRU/woDCS/03/log.txt  --model EBSNN_GRU --epochs 10 --csv_file /root/EBSNN-file/12/GRU/woDCS/03/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
 
 
 
 
 # 04
 
-CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/LSTM/woDCS/04/data.traffic --log_filename /root/EBSNN-file/12/LSTM/woDCS/04/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/12/LSTM/woDCS/04/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/GRU/woDCS/04/data.traffic --log_filename /root/EBSNN-file/12/GRU/woDCS/04/log.txt  --model EBSNN_GRU --epochs 10 --csv_file /root/EBSNN-file/12/GRU/woDCS/04/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
 
 
 
 
 # 05
 
-CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/LSTM/woDCS/05/data.traffic --log_filename /root/EBSNN-file/12/LSTM/woDCS/05/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/12/LSTM/woDCS/05/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/GRU/woDCS/05/data.traffic --log_filename /root/EBSNN-file/12/GRU/woDCS/05/log.txt  --model EBSNN_GRU --epochs 10 --csv_file /root/EBSNN-file/12/GRU/woDCS/05/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
 
 
 
 
 # 06
 
-CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/LSTM/woDCS/06/data.traffic --log_filename /root/EBSNN-file/12/LSTM/woDCS/06/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/12/LSTM/woDCS/06/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/GRU/woDCS/06/data.traffic --log_filename /root/EBSNN-file/12/GRU/woDCS/06/log.txt  --model EBSNN_GRU --epochs 10 --csv_file /root/EBSNN-file/12/GRU/woDCS/06/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
 
 
 
 
 # 07
 
-CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/LSTM/woDCS/07/data.traffic --log_filename /root/EBSNN-file/12/LSTM/woDCS/07/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/12/LSTM/woDCS/07/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/GRU/woDCS/07/data.traffic --log_filename /root/EBSNN-file/12/GRU/woDCS/07/log.txt  --model EBSNN_GRU --epochs 10 --csv_file /root/EBSNN-file/12/GRU/woDCS/07/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
 
 
 
 
 # 08
 
-CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/LSTM/woDCS/08/data.traffic --log_filename /root/EBSNN-file/12/LSTM/woDCS/08/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/12/LSTM/woDCS/08/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/GRU/woDCS/08/data.traffic --log_filename /root/EBSNN-file/12/GRU/woDCS/08/log.txt  --model EBSNN_GRU --epochs 10 --csv_file /root/EBSNN-file/12/GRU/woDCS/08/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
 
 
 
 
 # 09
 
-CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/LSTM/woDCS/09/data.traffic --log_filename /root/EBSNN-file/12/LSTM/woDCS/09/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/12/LSTM/woDCS/09/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/GRU/woDCS/09/data.traffic --log_filename /root/EBSNN-file/12/GRU/woDCS/09/log.txt  --model EBSNN_GRU --epochs 10 --csv_file /root/EBSNN-file/12/GRU/woDCS/09/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
 
 
 
 
 # 10
 
-CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/LSTM/woDCS/10/data.traffic --log_filename /root/EBSNN-file/12/LSTM/woDCS/10/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/12/LSTM/woDCS/10/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/GRU/woDCS/10/data.traffic --log_filename /root/EBSNN-file/12/GRU/woDCS/10/log.txt  --model EBSNN_GRU --epochs 10 --csv_file /root/EBSNN-file/12/GRU/woDCS/10/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
 
 
 
 
 # 11
 
-CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/LSTM/woDCS/11/data.traffic --log_filename /root/EBSNN-file/12/LSTM/woDCS/11/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/12/LSTM/woDCS/11/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/GRU/woDCS/11/data.traffic --log_filename /root/EBSNN-file/12/GRU/woDCS/11/log.txt  --model EBSNN_GRU --epochs 10 --csv_file /root/EBSNN-file/12/GRU/woDCS/11/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
 
 
 
 
 # 12
 
-CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/LSTM/woDCS/12/data.traffic --log_filename /root/EBSNN-file/12/LSTM/woDCS/12/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/12/LSTM/woDCS/12/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/GRU/woDCS/12/data.traffic --log_filename /root/EBSNN-file/12/GRU/woDCS/12/log.txt  --model EBSNN_GRU --epochs 10 --csv_file /root/EBSNN-file/12/GRU/woDCS/12/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
 
 
 
 
 # 13
 
-CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/LSTM/woDCS/13/data.traffic --log_filename /root/EBSNN-file/12/LSTM/woDCS/13/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/12/LSTM/woDCS/13/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/GRU/woDCS/13/data.traffic --log_filename /root/EBSNN-file/12/GRU/woDCS/13/log.txt  --model EBSNN_GRU --epochs 10 --csv_file /root/EBSNN-file/12/GRU/woDCS/13/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
 
 
 
 
 # 14
 
-CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/LSTM/woDCS/14/data.traffic --log_filename /root/EBSNN-file/12/LSTM/woDCS/14/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/12/LSTM/woDCS/14/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/GRU/woDCS/14/data.traffic --log_filename /root/EBSNN-file/12/GRU/woDCS/14/log.txt  --model EBSNN_GRU --epochs 10 --csv_file /root/EBSNN-file/12/GRU/woDCS/14/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
 
 
 
 
 # 15
 
-CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/LSTM/woDCS/15/data.traffic --log_filename /root/EBSNN-file/12/LSTM/woDCS/15/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/12/LSTM/woDCS/15/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/GRU/woDCS/15/data.traffic --log_filename /root/EBSNN-file/12/GRU/woDCS/15/log.txt  --model EBSNN_GRU --epochs 10 --csv_file /root/EBSNN-file/12/GRU/woDCS/15/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
 
 
 
 
 # 16
 
-CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/LSTM/woDCS/16/data.traffic --log_filename /root/EBSNN-file/12/LSTM/woDCS/16/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/12/LSTM/woDCS/16/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/GRU/woDCS/16/data.traffic --log_filename /root/EBSNN-file/12/GRU/woDCS/16/log.txt  --model EBSNN_GRU --epochs 10 --csv_file /root/EBSNN-file/12/GRU/woDCS/16/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
 
 
 
 
 # 17
 
-CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/LSTM/woDCS/17/data.traffic --log_filename /root/EBSNN-file/12/LSTM/woDCS/17/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/12/LSTM/woDCS/17/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/GRU/woDCS/17/data.traffic --log_filename /root/EBSNN-file/12/GRU/woDCS/17/log.txt  --model EBSNN_GRU --epochs 10 --csv_file /root/EBSNN-file/12/GRU/woDCS/17/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
 
 
 
 
 # 18
 
-CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/LSTM/woDCS/18/data.traffic --log_filename /root/EBSNN-file/12/LSTM/woDCS/18/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/12/LSTM/woDCS/18/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/GRU/woDCS/18/data.traffic --log_filename /root/EBSNN-file/12/GRU/woDCS/18/log.txt  --model EBSNN_GRU --epochs 10 --csv_file /root/EBSNN-file/12/GRU/woDCS/18/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
 
 
 
 
 # 19
 
-CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/LSTM/woDCS/19/data.traffic --log_filename /root/EBSNN-file/12/LSTM/woDCS/19/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/12/LSTM/woDCS/19/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/GRU/woDCS/19/data.traffic --log_filename /root/EBSNN-file/12/GRU/woDCS/19/log.txt  --model EBSNN_GRU --epochs 10 --csv_file /root/EBSNN-file/12/GRU/woDCS/19/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
 
 
 
 
 # 20
 
-CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/LSTM/woDCS/20/data.traffic --log_filename /root/EBSNN-file/12/LSTM/woDCS/20/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/12/LSTM/woDCS/20/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/12/GRU/woDCS/20/data.traffic --log_filename /root/EBSNN-file/12/GRU/woDCS/20/log.txt  --model EBSNN_GRU --epochs 10 --csv_file /root/EBSNN-file/12/GRU/woDCS/20/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
 
 
