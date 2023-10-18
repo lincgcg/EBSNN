b4d0fd2ba1080d5cf8ed479e98d8c622fa9796ba
“lincgcg <chunganglin@gmail.com>
2023-10-18 22:46:35 +0800

    debug

---
+++ 17_woDCS_LSTM.sh
@@ -0,0 +1,140 @@
+
+
+# 01
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/17/LSTM/woDCS/01/data.traffic --log_filename /root/EBSNN-file/17/LSTM/woDCS/01/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/17/LSTM/woDCS/01/result.csv --labels 'AIM,email-client,facebook,ftps,gmail,hangout,ICQ,netflix,scp,sftp,skype,spotify,vimeo,voipbuster,youtube,Tor,Torrent'
+
+
+
+
+# 02
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/17/LSTM/woDCS/02/data.traffic --log_filename /root/EBSNN-file/17/LSTM/woDCS/02/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/17/LSTM/woDCS/02/result.csv --labels 'AIM,email-client,facebook,ftps,gmail,hangout,ICQ,netflix,scp,sftp,skype,spotify,vimeo,voipbuster,youtube,Tor,Torrent'
+
+
+
+
+# 03
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/17/LSTM/woDCS/03/data.traffic --log_filename /root/EBSNN-file/17/LSTM/woDCS/03/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/17/LSTM/woDCS/03/result.csv --labels 'AIM,email-client,facebook,ftps,gmail,hangout,ICQ,netflix,scp,sftp,skype,spotify,vimeo,voipbuster,youtube,Tor,Torrent'
+
+
+
+
+# 04
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/17/LSTM/woDCS/04/data.traffic --log_filename /root/EBSNN-file/17/LSTM/woDCS/04/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/17/LSTM/woDCS/04/result.csv --labels 'AIM,email-client,facebook,ftps,gmail,hangout,ICQ,netflix,scp,sftp,skype,spotify,vimeo,voipbuster,youtube,Tor,Torrent'
+
+
+
+
+# 05
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/17/LSTM/woDCS/05/data.traffic --log_filename /root/EBSNN-file/17/LSTM/woDCS/05/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/17/LSTM/woDCS/05/result.csv --labels 'AIM,email-client,facebook,ftps,gmail,hangout,ICQ,netflix,scp,sftp,skype,spotify,vimeo,voipbuster,youtube,Tor,Torrent'
+
+
+
+
+# 06
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/17/LSTM/woDCS/06/data.traffic --log_filename /root/EBSNN-file/17/LSTM/woDCS/06/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/17/LSTM/woDCS/06/result.csv --labels 'AIM,email-client,facebook,ftps,gmail,hangout,ICQ,netflix,scp,sftp,skype,spotify,vimeo,voipbuster,youtube,Tor,Torrent'
+
+
+
+
+# 07
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/17/LSTM/woDCS/07/data.traffic --log_filename /root/EBSNN-file/17/LSTM/woDCS/07/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/17/LSTM/woDCS/07/result.csv --labels 'AIM,email-client,facebook,ftps,gmail,hangout,ICQ,netflix,scp,sftp,skype,spotify,vimeo,voipbuster,youtube,Tor,Torrent'
+
+
+
+
+# 08
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/17/LSTM/woDCS/08/data.traffic --log_filename /root/EBSNN-file/17/LSTM/woDCS/08/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/17/LSTM/woDCS/08/result.csv --labels 'AIM,email-client,facebook,ftps,gmail,hangout,ICQ,netflix,scp,sftp,skype,spotify,vimeo,voipbuster,youtube,Tor,Torrent'
+
+
+
+
+# 09
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/17/LSTM/woDCS/09/data.traffic --log_filename /root/EBSNN-file/17/LSTM/woDCS/09/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/17/LSTM/woDCS/09/result.csv --labels 'AIM,email-client,facebook,ftps,gmail,hangout,ICQ,netflix,scp,sftp,skype,spotify,vimeo,voipbuster,youtube,Tor,Torrent'
+
+
+
+
+# 10
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/17/LSTM/woDCS/10/data.traffic --log_filename /root/EBSNN-file/17/LSTM/woDCS/10/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/17/LSTM/woDCS/10/result.csv --labels 'AIM,email-client,facebook,ftps,gmail,hangout,ICQ,netflix,scp,sftp,skype,spotify,vimeo,voipbuster,youtube,Tor,Torrent'
+
+
+
+
+# 11
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/17/LSTM/woDCS/11/data.traffic --log_filename /root/EBSNN-file/17/LSTM/woDCS/11/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/17/LSTM/woDCS/11/result.csv --labels 'AIM,email-client,facebook,ftps,gmail,hangout,ICQ,netflix,scp,sftp,skype,spotify,vimeo,voipbuster,youtube,Tor,Torrent'
+
+
+
+
+# 12
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/17/LSTM/woDCS/12/data.traffic --log_filename /root/EBSNN-file/17/LSTM/woDCS/12/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/17/LSTM/woDCS/12/result.csv --labels 'AIM,email-client,facebook,ftps,gmail,hangout,ICQ,netflix,scp,sftp,skype,spotify,vimeo,voipbuster,youtube,Tor,Torrent'
+
+
+
+
+# 13
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/17/LSTM/woDCS/13/data.traffic --log_filename /root/EBSNN-file/17/LSTM/woDCS/13/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/17/LSTM/woDCS/13/result.csv --labels 'AIM,email-client,facebook,ftps,gmail,hangout,ICQ,netflix,scp,sftp,skype,spotify,vimeo,voipbuster,youtube,Tor,Torrent'
+
+
+
+
+# 14
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/17/LSTM/woDCS/14/data.traffic --log_filename /root/EBSNN-file/17/LSTM/woDCS/14/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/17/LSTM/woDCS/14/result.csv --labels 'AIM,email-client,facebook,ftps,gmail,hangout,ICQ,netflix,scp,sftp,skype,spotify,vimeo,voipbuster,youtube,Tor,Torrent'
+
+
+
+
+# 15
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/17/LSTM/woDCS/15/data.traffic --log_filename /root/EBSNN-file/17/LSTM/woDCS/15/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/17/LSTM/woDCS/15/result.csv --labels 'AIM,email-client,facebook,ftps,gmail,hangout,ICQ,netflix,scp,sftp,skype,spotify,vimeo,voipbuster,youtube,Tor,Torrent'
+
+
+
+
+# 16
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/17/LSTM/woDCS/16/data.traffic --log_filename /root/EBSNN-file/17/LSTM/woDCS/16/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/17/LSTM/woDCS/16/result.csv --labels 'AIM,email-client,facebook,ftps,gmail,hangout,ICQ,netflix,scp,sftp,skype,spotify,vimeo,voipbuster,youtube,Tor,Torrent'
+
+
+
+
+# 17
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/17/LSTM/woDCS/17/data.traffic --log_filename /root/EBSNN-file/17/LSTM/woDCS/17/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/17/LSTM/woDCS/17/result.csv --labels 'AIM,email-client,facebook,ftps,gmail,hangout,ICQ,netflix,scp,sftp,skype,spotify,vimeo,voipbuster,youtube,Tor,Torrent'
+
+
+
+
+# 18
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/17/LSTM/woDCS/18/data.traffic --log_filename /root/EBSNN-file/17/LSTM/woDCS/18/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/17/LSTM/woDCS/18/result.csv --labels 'AIM,email-client,facebook,ftps,gmail,hangout,ICQ,netflix,scp,sftp,skype,spotify,vimeo,voipbuster,youtube,Tor,Torrent'
+
+
+
+
+# 19
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/17/LSTM/woDCS/19/data.traffic --log_filename /root/EBSNN-file/17/LSTM/woDCS/19/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/17/LSTM/woDCS/19/result.csv --labels 'AIM,email-client,facebook,ftps,gmail,hangout,ICQ,netflix,scp,sftp,skype,spotify,vimeo,voipbuster,youtube,Tor,Torrent'
+
+
+
+
+# 20
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/EBSNN-file/17/LSTM/woDCS/20/data.traffic --log_filename /root/EBSNN-file/17/LSTM/woDCS/20/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/EBSNN-file/17/LSTM/woDCS/20/result.csv --labels 'AIM,email-client,facebook,ftps,gmail,hangout,ICQ,netflix,scp,sftp,skype,spotify,vimeo,voipbuster,youtube,Tor,Torrent'
+
+
