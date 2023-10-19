8d8f607ef1369f7cfd29ec84f249ede78e38cba6
“lincgcg <chunganglin@gmail.com>
2023-10-19 14:15:15 +0800

    debug

---
+++ 04.sh
@@ -0,0 +1,47 @@
+
+
+
+
+# LSTM 15 16 17 18 19 20
+
+
+
+# 15
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/autodl-tmp/EBSNN-file/12/LSTM/DCS/15/data.traffic --log_filename /root/autodl-tmp/EBSNN-file/12/LSTM/DCS/15/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/autodl-tmp/EBSNN-file/12/LSTM/DCS/15/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+
+
+
+# 16
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/autodl-tmp/EBSNN-file/12/LSTM/DCS/16/data.traffic --log_filename /root/autodl-tmp/EBSNN-file/12/LSTM/DCS/16/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/autodl-tmp/EBSNN-file/12/LSTM/DCS/16/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+
+
+
+
+# 17
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/autodl-tmp/EBSNN-file/12/LSTM/DCS/17/data.traffic --log_filename /root/autodl-tmp/EBSNN-file/12/LSTM/DCS/17/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/autodl-tmp/EBSNN-file/12/LSTM/DCS/17/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+
+
+
+
+# 18
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/autodl-tmp/EBSNN-file/12/LSTM/DCS/18/data.traffic --log_filename /root/autodl-tmp/EBSNN-file/12/LSTM/DCS/18/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/autodl-tmp/EBSNN-file/12/LSTM/DCS/18/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+
+
+
+
+# 19
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/autodl-tmp/EBSNN-file/12/LSTM/DCS/19/data.traffic --log_filename /root/autodl-tmp/EBSNN-file/12/LSTM/DCS/19/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/autodl-tmp/EBSNN-file/12/LSTM/DCS/19/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+
+
+
+
+# 20
+
+CUDA_VISIBLE_DEVICES=0 python3 train.py --flow --filename /root/autodl-tmp/EBSNN-file/12/LSTM/DCS/20/data.traffic --log_filename /root/autodl-tmp/EBSNN-file/12/LSTM/DCS/20/log.txt  --model EBSNN_LSTM --epochs 10 --csv_file /root/autodl-tmp/EBSNN-file/12/LSTM/DCS/20/result.csv --labels 'vpn-P2P,vpn-FT,Chat,vpn-Chat,vpn-Email,Streaming,P2P,VoIP,FT,vpn-VoIP,vpn-Streaming,Email'
+
+
